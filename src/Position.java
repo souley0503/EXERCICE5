1ce7174aca9f66e848f13af61ec04023cbb6fee7
Lenovo <sleyne@gmail.com>
2026-05-11 21:52:49 +0000

    commit initial

---
+++ src/Position.java
@@ -0,0 +1,11 @@
+public class Position {
+    public int row;    // Doit être public
+    public int column; // Doit être public
+
+    //constructeur
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+}
